adb56091768e51a9733befaae1bd785747644b1c
kinnera <kinnera.b2@gmail.com>
2017-08-10 12:31:16 -0400

    beautify

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,71 +20,76 @@ var dash = "_";
 //         icon = new ImageIcon(imageName);
 //     }
 function newGame() {
-      guessesLeft = 5;
-      letters = [];
-      randomword = arr[Math.floor(Math.random() * arr.length)];
-      console.log( randomword);
-      var l = randomword.length; //counting length of word choosen by computer.
-    	 userword = dash.repeat(l);
-    	console.log(userword);      
-    }
-function getindex(word, character) { //to get position of character in a word
-      var indices = []; //taking empty straing to store positions.
-      for (i = 0; i <word.length; i++) //if 
-      {
-        if (word[i] === character) indices.push(i); //tp push input to word if both characters are same.
-      }
-      return indices;
-    }
-    document.onkeyup = function(event) 
+  guessesLeft = 5;
+  letters = [];
+  randomword = arr[Math.floor(Math.random() * arr.length)];
+  console.log(randomword);
+  var l = randomword.length; //counting length of word choosen by computer.
+  userword = dash.repeat(l);
+  console.log(userword);
+}
+
+function getindex(word, character) //to get position of character in a word
+{ 
+  var indices = []; //taking empty straing to store positions.
+  for (i = 0; i <word.length; i++) //if 
+  {
+    if (word[i] == character) indices.push(i); //to push input to word if both characters are same.
+  }
+  return indices;
+}
+document.onkeyup = function(event) 
+{
+  var userinput = event.key;
+  console.log(userinput);
+  var inputlower = (userinput.toLowerCase());
+  if (letters.indexOf(inputlower) == -1) 
+  {
+    letters.push(inputlower); //pushungletter
+    var index = getindex(randomword, inputlower);
+    console.log(index);
+    if (index.length != 0) 
     {
-      var userinput = event.key;
-      console.log(userinput);
-      var inputlower = (userinput.toLowerCase());
-      if (letters.indexOf(inputlower) == -1)
-       {
-        letters.push(inputlower); //pushungletter
-        var index = getindex(randomword, inputlower);
-        console.log(index);
-        if (index.length != 0) 
-        {
-          for (i = 0; i <index.length; i++)
-          {
-          	var splitword = userword.split(""); //splitword is to create an array for the string
-            splitword[index[i]] = inputlower;
-            // console.log(splitword);
-            userword = splitword.join("");
-            console.log(userword);
-          }
-        } 
-        else {
-          guessesLeft--;
-          // console.log(guessesLeft);
-        }
-        if (guessesLeft == 0) {
-          alert("gameover");
-          losses++;
-          var gameOver = "you lost!!"
-          // document.querySelector("#endgame").innerHTML = gameOver;
-          newGame();
-        }
-        if (userword === randomword) {
-          win++;
-          alert("you Won!!");
-          var gameOver = "you won!!"
-      	  // document.querySelector("#endgame").innerHTML = gameOver;
-      	  newGame();
-        }
-      } else {
-        alert("you already guessed letter!!");
+      for (i = 0; i <index.length; i++) 
+      {
+        var splitword = userword.split(""); //splitword is to create an array for the string
+        splitword[index[i]] = inputlower;
+        // console.log(splitword);
+        userword = splitword.join("");
+        console.log(userword);
       }
-      var score =
-        "<p>Guess what word i am thinking : " + userword.split("").join(" ") + "</p>" +
-        "<p>Win: " + win + "</p>" +
-        "<p>loss: " + losses + "</p>" +
-        "<p>guess: " + guessesLeft + "</p>" +
-        "<p>Guesses so far : " + letters + "</p>";
-      document.querySelector("#game").innerHTML = score;
+    }
+    else 
+    {
+      guessesLeft--;
+      // console.log(guessesLeft);
+    }
+    if (guessesLeft == 0) {
+      alert("gameover");
+      losses++;
+      var gameOver = "you lost!!"
+      // document.querySelector("#endgame").innerHTML = gameOver;
+      newGame();
+    }
+    if (userword === randomword)
+    {
+      win++;
+      alert("you Won!!");
+      var gameOver = "you won!!"
+      // document.querySelector("#endgame").innerHTML = gameOver;
+      newGame();
+    }
+  } else 
+  {
+    alert("you already guessed letter!!");
+  }
+  var score =
+    "<p>Guess what word i am thinking : " + userword.split("").join(" ") + "</p>" +
+    "<p>Win: " + win + "</p>" +
+    "<p>loss: " + losses + "</p>" +
+    "<p>guess: " + guessesLeft + "</p>" +
+    "<p>Guesses so far : " + letters + "</p>";
+  document.querySelector("#game").innerHTML = score;
 };
 // newGame();
-document.getElementById("button").onclick =newGame();
+document.getElementById("button").onclick = newGame();
